75ebc99099d113c74ff657b12003aba3041ed433
AndreySlabov <andrey.slabov@devexpress.com>
2023-04-14 17:59:39 +0400

    format

--- CS/ListBoxFilteringUsingSearchPanel/MainWindow.xaml.cs
+++ CS/ListBoxFilteringUsingSearchPanel/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using System.Windows;
 
 namespace ListBoxFilteringUsingSearchPanel {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            listBox.ItemsSource = Stuff.GetStuff();
+            listBox.ItemsSource = Staff.GetStaff();
         }
     }
 }
